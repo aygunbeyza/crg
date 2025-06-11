ab2a910a1a5d79ca214beffcbcd15d9fb05165d8
Beyza Aygün <beyzaaygun@Beyza-MacBook-Air-5.local>
2025-06-11 17:44:51 +0200

    added mop

--- long_read_qc.sh
+++ long_read_qc.sh
@@ -1,2 +1,11 @@
 #for nanoplot
 NanoPlot --ubam /users/project/gencode_006070/jlagarde/nanopore/sequencing_runs/runs/20240115/20240115_BLaER_T0_sc10xONT/20240115_BLaER_T0_sc10xONT/20240115_1628_X1_FAX02366_794829ec/FAX02366_20240115_1632_794829ec.bam -o /users/rg/baygun/nanoplot/nanoplot_sample_1 --N50
+#for mop4 (need nextflow)
+git clone --depth 1 --recurse-submodules https://github.com/biocorecrg/master_of_pores.git
+#for running
+nextflow run biocorecrg/master_of_pores \
+-r MOP4-dev \
+--input my_file.fastq \
+--protocol direct_rna \
+--basecaller_guppy "none" \
+--outdir results_mop
