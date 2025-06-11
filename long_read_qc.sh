77d2ed7c517709db972a675ee104e5bb2e143c50
Beyza Aygün <beyzaaygun@Beyza-MacBook-Air-5.local>
2025-06-11 18:36:52 +0200

    changed the code and added explanation

--- long_read_qc.sh
+++ long_read_qc.sh
@@ -2,5 +2,5 @@
 NanoPlot --ubam /users/project/gencode_006070/jlagarde/nanopore/sequencing_runs/runs/20240115/20240115_BLaER_T0_sc10xONT/20240115_BLaER_T0_sc10xONT/20240115_1628_X1_FAX02366_794829ec/FAX02366_20240115_1632_794829ec.bam -o /users/rg/baygun/nanoplot/nanoplot_sample_1 --N50
 #for mop4 (need nextflow)
 git clone --depth 1 --recurse-submodules https://github.com/biocorecrg/master_of_pores.git
-#for running
-nextflow run mop_preprocess.nf -with-docker -params-file params.yaml -profile local
+#for running ==> You can change the parameters either by changing params.config file or by feeding the parameters via command line:(bg for bagground)
+nextflow run mop_preprocess.nf -with-singularity > log.txt
