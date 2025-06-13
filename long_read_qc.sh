1a59cade5a4e3812de5a6d05aa42545109d52340
Beyza Aygün <beyzaaygun@Beyza-MacBook-Air-5.local>
2025-06-13 13:24:01 +0200

    added some more codes for long and short data

--- long_read_qc.sh
+++ long_read_qc.sh
@@ -1,6 +1,28 @@
-#for nanoplot
+#1.epi2me
+# 1.1.docker install
+sudo apt update
+sudo apt install -y docker.io
+sudo usermod -aG docker ${USER}
+sudo systemctl restart docker
+#1.2. nextflow install
+conda install bioconda::nextflow
+#1.3. run epi2me
+nextflow run epi2me-labs/wf-human-variation \
+-r v0.2.0 \                     # Workflows' varation
+--input a.bam \                
+--outdir results_epi2me \      
+-profile docker                # Docker profiliyle çalıştır (container kullan)
+
+
+#2. for nanoplot
 NanoPlot --ubam /users/project/gencode_006070/jlagarde/nanopore/sequencing_runs/runs/20240115/20240115_BLaER_T0_sc10xONT/20240115_BLaER_T0_sc10xONT/20240115_1628_X1_FAX02366_794829ec/FAX02366_20240115_1632_794829ec.bam -o /users/rg/baygun/nanoplot/nanoplot_sample_1 --N50
-#for mop4 (need nextflow)
-git clone --depth 1 --recurse-submodules https://github.com/biocorecrg/master_of_pores.git
+
+
+#3.for mop4 (need nextflow)
+#NOTE: Please install nextflow Nextflow and either Singularity or Docker before
+#3.1. for installing
+curl -s https://get.nextflow.io | bash
+git clone --depth 1 --recurse-submodules https://github.com/biocorecrg/MOP2.git
+cd MoP2; bash INSTALL.sh
 #for running ==> You can change the parameters either by changing params.config file or by feeding the parameters via command line:(bg for bagground)
 nextflow run mop_preprocess.nf -with-singularity > log.txt
--- short_read.sh
+++ short_read.sh
@@ -1,1 +1,3 @@
+
+#for qc
 fastqc /users/rg/sequencing_data/Silvia_Carbonell/2024-02-19_AAFC333M5/GUIGO_16/cH000_lib_02535AAF_ATGACGTCGC-ATCCTGACCT_R2_001.fastq.gz
